ec35d5c32f363c2ee81009a040f33889384543c9
SMO <odon0070@algonquinlive.com>
2018-03-20 18:45:39 -0400

    initial commit

--- app/src/main/java/com/example/smo/androidlabs/MainActivity.java
+++ app/src/main/java/com/example/smo/androidlabs/MainActivity.java
@@ -11,6 +11,7 @@ import android.content.Intent;
 public class MainActivity extends Activity {
     protected static final String ACTIVITY_NAME="MainActivity";
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
